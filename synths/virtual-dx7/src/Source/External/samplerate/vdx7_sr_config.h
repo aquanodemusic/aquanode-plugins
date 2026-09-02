ee7dc271c02e82328c61952f563e5a2a10908372
Aqua Node <node.aqua@gmail.com>
2026-09-02 18:11:22 +0200

    Delete synths/virtual-dx7/src directory

--- synths/virtual-dx7/src/Source/External/samplerate/vdx7_sr_config.h
+++
@@ -1,9 +0,0 @@
-/* Minimal config for vendored libsamplerate (SRC_SINC_FASTEST only).
-   Enabled via HAVE_CONFIG_H. Part of VDX7-Dexed. */
-#pragma once
-#define PACKAGE "libsamplerate"
-#define VERSION "0.2.2"
-#define ENABLE_SINC_FAST_CONVERTER 1
-#define HAVE_STDBOOL_H 1
-#define CPU_CLIPS_POSITIVE 0
-#define CPU_CLIPS_NEGATIVE 0
